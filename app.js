6b3f564c8b40ca7a1ca83c0f6d1c4e5979b24d6d
Lucas Bassetto <lucasfbassetto@gmail.com>
2024-03-25 14:10:56 -0300

    feat: add html and js features

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-document.querySelector('button').addEventListener('click', () => {
-    var query = document.querySelector('input').value;
-    axios.get('https://www.omdbapi.com/?t=' +query + '&apikey=9f6f9cdf').then(function (res) {
-        console.log(res.data);
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let movieTitle = document.querySelector('.title');
+let movieYear = document.querySelector('.year');
+let movieRuntime = document.querySelector('.runtime');
+let movieDirector = document.querySelector('.director');
+let movieActors = document.querySelector('.actors');
+let movieGenre = document.querySelector('.genre');
+let movieReleased = document.querySelector('.released');
+
+button.addEventListener('click', () => {
+    var query = input.value;
+    axios.get('https://www.omdbapi.com/?t=' + query + '&apikey=9f6f9cdf')
+        .then(function (response) {
+            movieTitle.innerHTML = response.data.Title;
+            movieYear.innerHTML = response.data.Year;
+            movieRuntime.innerHTML = response.data.Runtime;
+            movieDirector.innerHTML = response.data.Director;
+            movieActors.innerHTML = response.data.Actors;
+            movieGenre.innerHTML = response.data.Genre;
+            movieReleased.innerHTML = response.data.Released;
+            
+            console.log(response.data);
+        })
     });
-});
