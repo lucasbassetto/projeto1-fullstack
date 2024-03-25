6dc412a96cfc61e080fbb496a6965b705df73dc1
Lucas Bassetto <lucasfbassetto@gmail.com>
2024-03-25 15:48:08 -0300

    refactor: refactoring code

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-let input = document.querySelector('input');
-let button = document.querySelector('button');
-let movieTitle = document.querySelector('.title');
-let movieYear = document.querySelector('.year');
-let movieRuntime = document.querySelector('.runtime');
-let movieDirector = document.querySelector('.director');
-let movieActors = document.querySelector('.actors');
-let movieGenre = document.querySelector('.genre');
-let movieReleased = document.querySelector('.released');
+function querySelector(selector) {
+    return document.querySelector(selector);
+}
+
+let button = querySelector('button');
+let input = querySelector('input');
+
+
+let selectors = {
+    '.title': 'Title',
+    '.year': 'Year',
+    '.runtime': 'Runtime',
+    '.director': 'Director',
+    '.actors': 'Actors',
+    '.genre': 'Genre',
+    '.released': 'Released',
+    '.plot': 'Plot'
+};
 
 button.addEventListener('click', () => {
-    var query = input.value;
+    let query = input.value;
     axios.get('https://www.omdbapi.com/?t=' + query + '&apikey=9f6f9cdf')
         .then(function (response) {
-            movieTitle.innerHTML = response.data.Title;
-            movieYear.innerHTML = response.data.Year;
-            movieRuntime.innerHTML = response.data.Runtime;
-            movieDirector.innerHTML = response.data.Director;
-            movieActors.innerHTML = response.data.Actors;
-            movieGenre.innerHTML = response.data.Genre;
-            movieReleased.innerHTML = response.data.Released;
-            
-            console.log(response.data);
-        })
-    });
+            let data = response.data;
+            for (let selector in selectors) {
+                let prop = selectors[selector];
+                querySelector(selector).innerHTML = data[prop];
+            }
+            console.log(data);
+        });
+});
