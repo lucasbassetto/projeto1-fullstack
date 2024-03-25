5228ef88f24457acd04bce654993b4448ec0a32c
Lucas Bassetto <lucasfbassetto@gmail.com>
2024-03-25 16:04:23 -0300

    refactor: add error clause

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function querySelector(selector) {
 
 let button = querySelector('button');
 let input = querySelector('input');
+let errorDiv = querySelector('#error');
 
 
 let selectors = {
@@ -14,18 +15,33 @@ let selectors = {
     '.actors': 'Actors',
     '.genre': 'Genre',
     '.released': 'Released',
-    '.plot': 'Plot'
+    '.plot': 'Plot',
+    '.awards': 'Awards',
+    '.rating': 'imdbRating'
 };
 
 button.addEventListener('click', () => {
     let query = input.value;
+    if (!query) {
+        errorDiv.innerHTML = 'Por favor, insira um termo de busca.';
+        return;
+    }
+    errorDiv.innerHTML = '';
     axios.get('https://www.omdbapi.com/?t=' + query + '&apikey=9f6f9cdf')
         .then(function (response) {
+            if (response.data.Error) {
+                errorDiv.innerHTML = response.data.Error;
+                return;
+            }
             let data = response.data;
             for (let selector in selectors) {
                 let prop = selectors[selector];
                 querySelector(selector).innerHTML = data[prop];
             }
             console.log(data);
+        })
+        .catch(function (error) {
+            errorDiv.innerHTML = 'Ocorreu um erro ao buscar os dados do filme.';
+            console.error(error);
         });
 });
